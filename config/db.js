20ab71d83183dbf739e941f71f922a05c75511be
Datta2907 <datta598764@gmail.com>
2024-02-12 23:54:48 +0530

    updated nodemon version

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+console.log("testing node env", process.env.NODE_ENV);
 const connectDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_URI, {
