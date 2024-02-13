a138ae1d503774eb1947ec21e10861a7a39a7c70
Datta2907 <datta598764@gmail.com>
2024-02-13 19:44:26 +0530

    added cors files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectdb = require('./config/db');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 require('dotenv').config();
@@ -8,7 +9,11 @@ connectdb();
 app.use(express.json());
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/user',require('./routes/user'));
-
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://phone-comparor.onrender.com"]
+  })
+)
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('frontend/build'));
