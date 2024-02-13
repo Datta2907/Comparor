bbfc96a58b29d5a117ac9d489ad0a6816a828805
Datta2907 <datta598764@gmail.com>
2024-02-13 22:34:11 +0530

    removed cors and cors from package json

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectdb = require('./config/db');
-const cors = require('cors');
 const app = express();
 const path = require('path');
 require('dotenv').config();
@@ -10,18 +9,17 @@ app.use(express.json());
 -app.use(express.urlencoded({ extended: false}));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/user',require('./routes/user'));
-app.use(cors())
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   const allowedOrigins = ['http://localhost:3000', 'https://phone-comparor.onrender.com'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
-})
+});
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('frontend/build'));
