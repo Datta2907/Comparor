e989f3d972f1575d2bc87e7f4df960c0d3ecb2c1
Datta2907 <datta598764@gmail.com>
2024-02-13 21:27:38 +0530

    use origins in cors

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ require('dotenv').config();
 connectdb();
 
 app.use(express.json());
+-app.use(express.urlencoded({ extended: false}));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/user',require('./routes/user'));
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000","https://phone-comparor.onrender.com/"],
+}))
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('frontend/build'));
