0b6a3ffd35f2de99db9f10db81584a3051acfc7a
Datta2907 <datta598764@gmail.com>
2024-02-13 13:20:15 +0530

    backend deplyoment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectdb = require('./config/db');
 const app = express();
 const path = require('path');
-
+require('dotenv').config();
 connectdb();
 
 app.use(express.json());
@@ -10,14 +10,14 @@ app.use('/api/auth',require('./routes/auth'));
 app.use('/api/user',require('./routes/user'));
 
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('frontend/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-  }
-  
-  const PORT = process.env.PORT || 5000;
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}  
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
