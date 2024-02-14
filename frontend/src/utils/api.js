8de61a45f74e247153430fe765e7a0968304bb88
Datta2907 <datta598764@gmail.com>
2024-02-14 15:16:02 +0530

    cors cors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://phone-comparor-app.onrender.com/api',
+  baseURL: 'https://phone-comparor-app.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
