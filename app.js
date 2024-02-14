12a8da7b6ff5f146c861d4439b15c328d98b7441
Datta2907 <datta598764@gmail.com>
2024-02-14 13:27:52 +0530

    cors issues as mentioned by render.com

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ require('dotenv').config();
 connectdb();
 
 app.use(express.json());
--app.use(express.urlencoded({ extended: false}));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/user',require('./routes/user'));
-app.use(function(req, res, next) {
-  const allowedOrigins = ['http://localhost:3000', 'https://phone-comparor.onrender.com'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+// Set middleware of CORS 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://phone-comparor.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
   next();
 });
 if (process.env.NODE_ENV === 'production') {
