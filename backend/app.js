eab07d38a236e285db50ceae3c3f6e6e213a092f
Datta2907 <datta598764@gmail.com>
2024-02-16 12:58:12 +0530

    resolved cors error

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,10 @@ const app = express();
 const path = require('path');
 require('dotenv').config();
 connectdb();
-
-app.use(express.json());
-app.use('/auth',require('./routes/auth'));
-app.use('/user',require('./routes/user'));
-// Set middleware of CORS 
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://phone-comparor-web.onrender.com"
+    "https://phone-comparor-app.onrender.com"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -22,12 +17,16 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
+  res.setHeader("Access-Control-Expose-Headers", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Private-Network", true);
   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
   res.setHeader("Access-Control-Max-Age", 7200);
   next();
 });
+app.use(express.json());
+app.use('/auth',require('./routes/auth'));
+app.use('/user',require('./routes/user'));
 
 // needed in heroku
 // if (process.env.NODE_ENV === 'production') {
